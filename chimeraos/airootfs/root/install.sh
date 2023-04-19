c51021b81db2bcdd8e734a381794a7fbe830938b
Matthew Anderson <ruinairas1992@gmail.com>
2023-04-19 11:43:25 -0500

    Use official unstable branch

--- chimeraos/airootfs/root/install.sh
+++ chimeraos/airootfs/root/install.sh
@@ -68,7 +68,7 @@ if [ "${CHOICE}" == "local" ]; then
     frzr-deploy
     RESULT=$?
 else
-    frzr-deploy chimeraos/chimeraos:stable
+    frzr-deploy chimeraos/chimeraos:unstable
     RESULT=$?
 fi
 
